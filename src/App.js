e5bcc919a4489c60daae180365f0bc09208cdd82
Akshay <worklikeakshay@gmail.com>
2023-12-11 14:46:37 +0530

    Added Changes Draft 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import CardList from './Cardlist';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [popupMessage, setPopupMessage] = useState('');
+
+  const handleAddLink = async () => {
+    try {
+      const response = await fetch(`https://bookey-backend.vercel.app/api/addBookmark?url=${encodeURIComponent(url)}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const data = await response.json();
+
+      if (response.ok && data.success) {
+        setPopupMessage('Bookmark added successfully');
+        setUrl(''); // Clear the input field
+        // You can perform additional actions or reload the cards here if needed
+      } else {
+        setPopupMessage('Failed to add bookmark');
+      }
+    } catch (error) {
+      console.error('Error adding bookmark:', error.message);
+      setPopupMessage('Error adding bookmark');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="heading">Bookmarks</div>
+        <div className="components">
+          <div className="form">
+            <input
+              type="text"
+              className="form__input"
+              placeholder="Type anything..."
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
+          </div>
+          <div className="btn btn__primary" onClick={handleAddLink}>
+            <p>Add Link</p>
+          </div>
+        </div>
+
+        <CardList />
+
+        {popupMessage && (
+          <div className="popup">
+            <p>{popupMessage}</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/Cardlist.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+
+const CardList = () => {
+  const [bookmarks, setBookmarks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://bookey-backend.vercel.app/api/bookmarks');
+        const data = await response.json();
+        setBookmarks(data);
+      } catch (error) {
+        console.error('Error fetching bookmarks:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <ul className="cards">
+      {bookmarks.map((bookmark, index) => (
+        <li key={index}>
+          <a href={bookmark.url} className="card">
+            <img src={bookmark.imageUrl} className="card__image" alt="" />
+            <div className="card__overlay">
+              <div className="card__header">
+                <svg className="card__arc" xmlns="http://www.w3.org/2000/svg">
+                  <path />
+                </svg>
+                <img className="card__thumb" src={bookmark.imageUrl} alt="" />
+                <div className="card__header-text">
+                  <h3 className="card__title">{bookmark.title}</h3>
+                  <span className="card__status">
+                     {bookmark.name}
+                  </span>
+                </div>
+              </div>
+              <p className="card__description">{bookmark.description}</p>
+            </div>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default CardList;
